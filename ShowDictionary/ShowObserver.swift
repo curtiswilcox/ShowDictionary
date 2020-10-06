9638fc59697b2d802808d177a32572fc23d6ed8b
quillcox <Curtis.Wilcox@uvm.edu>
2020-10-06 13:22:57 -0400

    save image data to fall back on if internet connection is bad

--- ShowDictionary/ShowObserver.swift
+++ ShowDictionary/ShowObserver.swift
@@ -12,100 +12,93 @@ import Foundation
 import UIKit.UIImage
 
 class ShowObserver : ObservableObject {
-    var data: [ShowData] = []
-    private var numberCompleted: CGFloat = 0
-    @Published private(set) var percentCompleted: CGFloat = 0
-
-    init() {
-        getShows() { shows in
-            for show in shows {
-                let config = URLSessionConfiguration.default
-                config.timeoutIntervalForRequest = 30
-                config.timeoutIntervalForResource = 60
-                URLSession(configuration: config).dataTask(with: show.titleCardURL, completionHandler: { (data, response, error) in
-                    DispatchQueue.main.async {
-                        if let imageData = data, let img = UIImage(data: imageData) {
-                            self.data.append(ShowData(show, img))
-                        } else {
-                            self.data.append(ShowData(show)) // img is nil
-                        }
-                        self.numberCompleted += 1
-                        self.percentCompleted = self.numberCompleted / CGFloat(shows.count) * 100
-                        self.data.sort(by: <)
-                    }
-                }).resume()
+  var data: [ShowData] = []
+  private var numberCompleted: CGFloat = 0
+  @Published private(set) var percentCompleted: CGFloat = 0
+  
+  init() {
+    getShows() { shows in
+      for show in shows {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 30
+        config.timeoutIntervalForResource = 30
+        URLSession(configuration: config).dataTask(with: show.titleCardURL, completionHandler: { (data, response, error) in
+          DispatchQueue.main.async {
+            if let imageData = data, let img = UIImage(data: imageData) {
+              self.data.append(ShowData(show, img))
+              self.saveImageData(imageData, show: show.filename)
+            } else {
+              if let img = self.savedImage(show: show.filename) {
+                self.data.append(ShowData(show, img))
+              } else {
+                self.data.append(ShowData(show)) // img is nil
+              }
             }
-        }
+            self.numberCompleted += 1
+            self.percentCompleted = self.numberCompleted / CGFloat(shows.count) * 100
+            self.data.sort(by: <)
+          }
+        }).resume()
+      }
     }
-
-    func getShows(completion: @escaping ([Show]) -> ()) {
-        var shows: [Show] = []
-
-        Alamofire.request("https://wilcoxcurtis.com/show-dictionary/files/shows_\(Locale.current.languageCode ?? "en").json", method: .get).responseString() { response in
-            switch response.result {
-            case .success(var text):
-                text = text.replacingOccurrences(of: "<pre>", with: "").replacingOccurrences(of: "</pre>", with: "").trimmingCharacters(in: .whitespacesAndNewlines) // remove the pretty-print tags from HTML
-                
-                do {
-                    let decoder = JSONDecoder()
-                    shows = try decoder.decode([Show].self, from: Data(text.utf8)).sorted(by: <)
-                    completion(shows)
-//                    self.findFavoritedShows() { favs in
-//                        print(shows)
-//                        guard !shows.isEmpty else { return }
-//                        shows = shows.sorted(by: <)
-//                        for show in shows where favs?.contains(show.filename) ?? false {
-//                            show.hasFavoritedEpisodes = true
-//                        }
-//                        print(shows)
-//                        completion(shows)
-//                    }
-                } catch {
-                    completion([])
-                }
-            case .failure:
-                completion([])
-            }
+  }
+  
+  func getShows(completion: @escaping ([Show]) -> ()) {
+    var shows: [Show] = []
+    
+    Alamofire.request("https://wilcoxcurtis.com/show-dictionary/files/shows_\(Locale.current.languageCode ?? "en").json", method: .get).responseString() { response in
+      switch response.result {
+      case .success(var text):
+        text = text.replacingOccurrences(of: "<pre>", with: "").replacingOccurrences(of: "</pre>", with: "").trimmingCharacters(in: .whitespacesAndNewlines) // remove the pretty-print tags from HTML
+        
+        do {
+          let decoder = JSONDecoder()
+          shows = try decoder.decode([Show].self, from: Data(text.utf8)).sorted(by: <)
+          completion(shows)
+        } catch {
+          completion([])
         }
+      case .failure:
+        completion([])
+      }
+    }
+  }
+  
+  func saveImageData(_ data: Data, show: String) {
+    let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    let fileURL = URL(fileURLWithPath: "\(show)-title-card", relativeTo: directoryURL).appendingPathExtension("txt")
+    
+    if FileManager.default.fileExists(atPath: fileURL.path) {
+      do {
+        if try Data(contentsOf: fileURL) == data { /*print("\(show) file already exists!");*/ return }
+      } catch { /* doesn't matter */ }
     }
     
-    /*
-    func findFavoritedShows(completion: @escaping (Set<String>?) -> Void) {
-        guard signedIn else {
-//            print("iCloud account not available!")
-            completion(nil)
-            return
-        }
-                
-        let predicate = NSPredicate(value: true)
-        let query = CKQuery(recordType: "episodes", predicate: predicate)
-        let operation = CKQueryOperation(query: query)
-        operation.desiredKeys = ["filename"]
-        
-        var records = Set<String>()
-        operation.recordFetchedBlock = { record in
-            let filename = String(record["filename"]!)
-            records.insert(filename)
-        }
-        operation.queryCompletionBlock = { (cursor, error) in
-            if let cursor = cursor {
-                let newOperation = CKQueryOperation(cursor: cursor)
-                newOperation.recordFetchedBlock = operation.recordFetchedBlock
-                newOperation.queryCompletionBlock = operation.queryCompletionBlock
-                CKContainer(identifier: "iCloud.wilcoxcurtis.ShowDictionary").privateCloudDatabase.add(newOperation)
-            } else {
-                DispatchQueue.main.async {
-                    print(records)
-                    guard error == nil else {
-                        print(error!)
-                        completion(nil)
-                        return
-                    }
-                    completion(records)
-                }
-            }
-        }
-        CKContainer(identifier: "iCloud.wilcoxcurtis.ShowDictionary").privateCloudDatabase.add(operation)
+    do {
+      try data.write(to: fileURL)
+      print("\(show) file written!")
+    } catch {
+      // leave empty for now
     }
- */
+  }
+  
+  func savedImage(show: String) -> UIImage? {
+    let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    let fileURL = URL(fileURLWithPath: "\(show)-title-card", relativeTo: directoryURL).appendingPathExtension("txt")
+    
+    guard FileManager.default.fileExists(atPath: fileURL.path) else { /*print("\(show) file doesn't exist.");*/ return nil }
+    
+    do {
+      let data = try Data(contentsOf: fileURL)
+      if let image = UIImage(data: data) {
+//        print("\(show) file exists and loaded!")
+        return image
+      }
+//      print("\(show) file exists and NOT loaded!")
+      return nil
+    } catch {
+//      print("\(show) file had a problem loading!")
+      return nil
+    }
+  }
 }
