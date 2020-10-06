d663b75d8923afb811b7509f6f1e4a3eaa557e10
quillcox <Curtis.Wilcox@uvm.edu>
2020-10-05 22:11:23 -0400

    add a button for the CardView... oops

--- ShowDictionary/CharacterView.swift
+++ ShowDictionary/CharacterView.swift
@@ -10,59 +10,91 @@ import SwiftUI
 
 struct CharacterView: View {
   let show: Show
+  @State var characterSelected: (character: Show.Character?, showing: Bool) = (nil, false)
   
   var body: some View {
-    List {
-      ForEach(self.getSectionHeaders(), id: \.self) { header in
-        Section(header: Text(header)) {
-          ForEach(self.getCharacters().filter { $0.character.lastName.firstLetter() == header }, id: \.self) { character in
-            CharacterRow(show: self.show, character: character)
-          }
+    ZStack {
+      if let character = characterSelected.character {
+        let navTitle = "\(String(format: NSLocalizedString("Episodes with %@", comment: ""), character.character.fullName))"
+        let episodesToPass = epsWithChar(character, show: show)
+        NavigationLink(destination: EpisodeChooserView(navTitle: navTitle, show: self.show, episodes: episodesToPass), isActive: $characterSelected.showing) {
+          EmptyView()
+        }
+      }
+      GeometryReader { geometry in
+        ScrollView {
+          let width = geometry.size.width / 2.5
+          GridView(show: show, characterSelected: $characterSelected, width: width)
+            .onAppear { characterSelected = (nil, false) }
         }
       }
     }
-    .navigationBarTitle("character".localizeWithFormat(quantity: self.getCharacters().count).capitalized)
-  }
-  
-  private func getSectionHeaders() -> [String] {
-    return Set(getCharacters().map { $0.character.lastName.firstLetter().uppercased() }).sorted()
-  }
-  
-  private func getCharacters() -> [Show.Character] {
-    var characters = [Show.Character]()
-    for episode in self.show.episodes {
-      for character in episode.characters! {
-        characters.append(character)
-      }
-    }
-    return Set(characters).sorted(by: { $0.character < $1.character })
+    .navigationBarTitle("character".localizeWithFormat(quantity: getCharacters(show: show).count).capitalized)
   }
 }
 
 extension CharacterView {
-  struct CharacterRow: View {
-    let show: Show
-    let character: Show.Character
+  struct GridView: View {
+    @ObservedObject var show: Show
+    @Binding var characterSelected: (character: Show.Character?, showing: Bool)
+    let width: CGFloat
     
     var body: some View {
-      NavigationLink(destination: EpisodeChooserView(navTitle: "\(String(format: NSLocalizedString("Episodes with %@", comment: ""), character.character.fullName))", show: self.show, episodes: self.epsWithChar(character))) {
-        VStack(alignment: .leading) {
-          Text(character.character.fullName)
-          HStack {
-            SubText(character.actor.fullName)
-            Spacer()
-            SubText("episode".localizeWithFormat(quantity: self.getNumEps(character)).capitalized)
+      LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 2), spacing: 20) {
+        ForEach(getSectionHeaders(show: show), id: \.self) { header in
+          Section(header:
+                    HStack {
+                      VStack { Divider().padding(.horizontal) }
+                      Text(header).bold()
+                      VStack { Divider().padding(.horizontal) }
+                    }) {
+            ForEach(getCharacters(show: show).filter { $0.character.lastName.firstLetter() == header }, id: \.self) { character in
+              Button {
+                characterSelected = (character, true)
+              } label: {
+                CardView(width: width, vertAlignment: .top) {
+                  Text(character.character.fullName)
+                    .font(.callout)
+                    .bold()
+                    .foregroundColor(Color(UIColor.label))
+                    .padding(.top)
+                  SubText(character.actor.fullName)
+                  Spacer()
+                  Divider()
+                    .background(Color(UIColor.systemGray))
+                    .frame(width: width / 3)
+                    .padding(.all, 0)
+                  SubText("episode".localizeWithFormat(quantity: getNumEps(character, show: show)))
+                    .padding(.bottom)
+                }
+              }
+            }
           }
         }
       }
-    }
-    
-    private func getNumEps(_ character: Show.Character) -> Int {
-      return epsWithChar(character).count
-    }
-    
-    private func epsWithChar(_ character: Show.Character) -> [Episode] {
-      return show.episodes.filter { $0.characters?.contains(character) ?? false }
+      .padding(.horizontal)
     }
   }
+}
+
+fileprivate func getNumEps(_ character: Show.Character, show: Show) -> Int {
+  return epsWithChar(character, show: show).count
+}
+
+fileprivate func epsWithChar(_ character: Show.Character, show: Show) -> [Episode] {
+  return show.episodes.filter { $0.characters?.contains(character) ?? false }
+}
+
+fileprivate func getSectionHeaders(show: Show) -> [String] {
+  return Set(getCharacters(show: show).map { $0.character.lastName.firstLetter().uppercased() }).sorted()
+}
+
+fileprivate func getCharacters(show: Show) -> [Show.Character] {
+  var characters = [Show.Character]()
+  for episode in show.episodes {
+    for character in episode.characters! {
+      characters.append(character)
+    }
+  }
+  return Set(characters).sorted(by: { $0.character < $1.character })
 }
